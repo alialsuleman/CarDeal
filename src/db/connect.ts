2bc7712601220e69adcb74d2a7ebcbdec527b9b1
alialsuleman <alsulemanali08@gmail.com>
2025-08-05 20:59:15 +0300

    first commit4

--- src/db/connect.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose"
-
-export const connectDB = (url: string) => {
-    mongoose.set("strictQuery", false);
-    return mongoose.connect(url)
-} 
--- src/middleware/not-found.ts
+++
@@ -1,7 +0,0 @@
-import { Response, Request } from "express"
-
-export const notFound = (_: Request, res: Response) => res.status(404).send(
-    {
-        success: false,
-        message: 'Route does not exist!'
-    });
